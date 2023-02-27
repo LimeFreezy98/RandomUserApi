a3375deb79fb055cf66297d2958eb593b69e7f3c
LimeFreezy98 <89162212+LimeFreezy98@users.noreply.github.com>
2023-02-27 08:57:05 -0700

    Initial Commit

---
+++ 2nd code challange/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2nd code challange
+//
+//  Created by Vasiliy on 2/27/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
